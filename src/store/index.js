f84cfe49043f0963f5bb1ae19e7c0b471e56ea0e
zhanghao <1398351754@163.com>
2022-01-16 21:58:34 +0800

    fix: add privileged: true

--- src/store/index.js
+++ src/store/index.js
@@ -11,11 +11,11 @@ const state = {
 const mutations = {
     setUserBase(state,user){
         if(user.strid != undefined){
-            state.user['userid'] = user.strid
+            state.user.userid = user.strid
         }
 
         if(user.base != undefined){
-            state.user['base'] = user.base
+            state.user.base = user.base
         }
     },
     setToken(state,token){
